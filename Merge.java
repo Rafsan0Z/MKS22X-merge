f0914b57213086b9c7cf9fc82744ff52f05bf7c3
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-15 10:54:47 -0400

    second mergesort edited and a merge helper method added!

--- Merge.java
+++ Merge.java
@@ -9,10 +9,15 @@ public static void mergesort(int[] data, int lo, int hi){
   int point = (lo+hi)/2;
   mergesort(data,lo,point);
   mergesort(data,point+1,hi);
+  merge(data,lo,point,hi);
+}
+
+public static void merge(int[] data, int lo, int point, int hi){
+  
 }
 
 public static void main(String[] args){
-  
+
 }
 
 
