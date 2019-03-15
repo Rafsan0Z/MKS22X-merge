6f76d48750f0faca9f23ffe0d814692e167e1d0e
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-15 10:52:51 -0400

    mergesort edited and main method created!

--- Merge.java
+++ Merge.java
@@ -6,8 +6,13 @@ public static void mergesort(int[] data){
 
 public static void mergesort(int[] data, int lo, int hi){
   if(lo >= hi){return;}
+  int point = (lo+hi)/2;
   mergesort(data,lo,point);
   mergesort(data,point+1,hi);
+}
+
+public static void main(String[] args){
+  
 }
 
 
