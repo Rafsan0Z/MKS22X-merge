a3d11f0d19a48c52cc36ce1f2ab4a37ed350b8c0
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-15 10:51:23 -0400

    second mergesort edited!

--- Merge.java
+++ Merge.java
@@ -1,11 +1,13 @@
 public class Merge{
 
 public static void mergesort(int[] data){
-  
+
 }
 
 public static void mergesort(int[] data, int lo, int hi){
-
+  if(lo >= hi){return;}
+  mergesort(data,lo,point);
+  mergesort(data,point+1,hi);
 }
 
 
