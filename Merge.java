a3c0d8dbd02224e3399a992cc663161a78b4c333
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-16 12:27:19 -0400

    Optimized Mergesort added!

--- Merge.java
+++ Merge.java
@@ -5,6 +5,10 @@ public static void mergesort(int[] data){
   mergesort(data,0,data.length-1);
 }
 
+public static void Mergesort(int[] data){
+  
+}
+
 public static void mergesort(int[] data, int lo, int hi){
   if(lo >= hi){return;}
   int point = (lo+hi)/2;
@@ -20,7 +24,7 @@ public static void merge(int[] data, int lo, int point, int hi){
 }
 
 public static void mergesort(int[] data, int[] temp, int lo, int hi){
-  
+
 }
 
 public static void main(String[] args){
