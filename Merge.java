07f834595d630ccc87857b7088e652ffc0819eaa
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-20 22:11:44 -0400

    mergesort itself changed!

--- Merge.java
+++ Merge.java
@@ -77,15 +77,15 @@ public static void insertionSort(int[] ary, int lo, int hi) {
   }
 }
 
-public static void mergesort(int[] data, int[] temp, int lo, int hi){
+public static void mergesort(int[] data, int lo, int hi){
   if(hi == lo){return;}
   int point = (hi+lo)/2;
   int[] LSort = new int[point+1];
   for(int i = 0; i <= point; i++){
     LSort[i] = data[i];
   }
-  mergesort(data,temp,lo,point);
-  mergesort(data,temp,point+1,hi);
+  mergesort(LSort,0,point);
+  mergesort(RSort,0,hi);
   merge(LSort,RSort);
 }
 
